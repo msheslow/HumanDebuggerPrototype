0f642028f0984468d0a15c7dea5008ec7c10174d
Michael <msheslow@gmail.com>
2023-10-22 12:50:11 -0600

    Adds more dynamic Goal, KPI, Hypothesis, Experiment functionality

--- HumanDebuggerPrototype/ContentView.swift
+++ HumanDebuggerPrototype/ContentView.swift
@@ -8,22 +8,68 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var goal = ""
+    @State private var goal: String = ""
+    @State private var kpi: String = ""
+    @State private var hypothesis: String = ""
+    @State private var experiment: String = ""
     
+    @State private var isGoalSaved: Bool = false
+    @State private var isKPISaved: Bool = false
+    @State private var isHypothesisSaved: Bool = false
+    @State private var isExperimentSaved: Bool = false
+
     var body: some View {
         VStack {
-            TextField("Enter your goal", text: $goal)
-                .padding()
-                .border(Color.black)
+            HStack {
+                Text("Goal: \(isGoalSaved ? goal : "Not Set") | KPI: \(isKPISaved ? kpi : "Not Set")")
+                    .font(.headline)
+                    .padding()
+            }
+            .background(Color.gray.opacity(0.5))
             
-            Text("Your goal is: \(goal)")
+            if !isGoalSaved {
+                HStack {
+                    TextField("Enter Your Goal", text: $goal)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    Button(action: { self.isGoalSaved = true }) {
+                        Text("Save")
+                    }
+                }
+                .padding()
+            }
+            
+            if isGoalSaved && !isKPISaved {
+                HStack {
+                    TextField("Enter KPI for your Goal", text: $kpi)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    Button(action: { self.isKPISaved = true }) {
+                        Text("Save")
+                    }
+                }
+                .padding()
+            }
+            
+            if isKPISaved && !isHypothesisSaved {
+                HStack {
+                    TextField("Enter Your Hypothesis", text: $hypothesis)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    Button(action: { self.isHypothesisSaved = true }) {
+                        Text("Save")
+                    }
+                }
+                .padding()
+            }
+            
+            if isHypothesisSaved && !isExperimentSaved {
+                HStack {
+                    TextField("Enter Your Experiment", text: $experiment)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    Button(action: { self.isExperimentSaved = true }) {
+                        Text("Save")
+                    }
+                }
+                .padding()
+            }
         }
-        .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
     }
 }
